e65fe0da385b47d56079232318785a6f62dbaad3
Atwinenickson <atwiinenicxon@gmail.com>
2020-09-08 17:33:48 +0300

    added a test api for data

---
+++ core/views.py
@@ -0,0 +1,21 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from rest_framework.response import  Response
+from rest_framework.views import APIView
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+        'name':'john',
+        'age':23
+    }
+        return Response(data)
+
+# def test_view(request):
+#     data = {
+#         'name':'john',
+#         'age':23
+#     }
+#     return JsonResponse(data)
+
