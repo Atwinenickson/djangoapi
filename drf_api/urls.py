a29c97769f61a0b44e5f6ab76b03a28f621397a3
Atwinenickson <atwiinenicxon@gmail.com>
2020-09-08 18:40:53 +0300

    add saerializers

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from core.views import TestView
 
 urlpatterns = [
-    path('^api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', TestView.as_view(), name='test')
 ]
