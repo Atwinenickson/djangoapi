bdbd5491ed8d69ac4cf0d482259736b0a06e5680
Atwinenickson <atwiinenicxon@gmail.com>
2020-09-14 15:26:40 +0300

    added the details class models

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import article_list, article_detail, ClubAPIView
+from .views import article_list, article_detail, ClubAPIView, ClubDetails
 
 urlpatterns = [
     path('article/', article_list),
     path('detail/<int:pk>/', article_detail),
-    path('club/', ClubAPIView.as_view(), name='club')
+    path('club/', ClubAPIView.as_view(), name='club'),
+    path('clubOne', ClubDetails.as_view(), name='clubone')
 ]
